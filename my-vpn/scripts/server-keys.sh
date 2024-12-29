5881332f9c0b4dcb2e5284641f28d8710eb9c51d
zharkaron <daron.valdiviaceron@gmail.com>
2024-12-29 00:07:13 +0000

    Initial commit

--- my-vpn/scripts/server-keys.sh
+++ my-vpn/scripts/server-keys.sh
@@ -8,7 +8,7 @@ check_and_install_package(){
 	if dpkg -l | grep -q "$package"; then
 		echo "$package is already installed."
 	else
-		echo "$package is not installed. installing now ..."
+		echo "$package is not installed. Installing now ..."
 		apt-get update
 		apt-get install -y $package
 		echo "$package installation complete."
@@ -21,24 +21,24 @@ check_and_install_package $PACKAGE_2
 mkdir -p /etc/openvpn/easy-rsa
 cp -r /usr/share/easy-rsa/* /etc/openvpn/easy-rsa
 cd /etc/openvpn/easy-rsa
-ca="build-ca"
-crt="gen-req server nopass"
-key="sign-req server server"
-dh="gen-dh"
-certificate(){
-	local command=$1
-	./easyrsa $command
-}
-certificate $ca
-certificate $crt
-certificate $key
-certificate $dh
 
+# Initialize PKI and generate keys
+./easyrsa init-pki
+./easyrsa build-ca
+./easyrsa gen-req server nopass
+./easyrsa sign-req server server
+./easyrsa gen-dh
+
+# Generate TLS authentication key
 openvpn --genkey secret ta.key
-file_path= "~/docker/my-vpn/config
 
-cp pki/issued/server.crt $file_path
-cp pki/private/server.key $file_path
-cp ta.key $file_path
-cp pki/ca.crt $file_path
-cp dh.pem $file_path
+# Ensure the target directory exists
+file_path="~/docker/my-vpn/config"
+# Copy files to the target directory
+cp /etc/openvpn/easy-rsa/pki/issued/server.crt $file_path
+cp /etc/openvpn/easy-rsa/pki/private/server.key $file_path
+cp /etc/openvpn/easy-rsa/ta.key $file_path
+cp /etc/openvpn/easy-rsa/pki/ca.crt $file_path
+cp /etc/openvpn/easy-rsa/pki/dh.pem $file_path
+
+echo "All keys and certificates have been generated and copied to $file_path."
